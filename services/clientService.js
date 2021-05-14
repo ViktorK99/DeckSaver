0e30cb3ec583c60f99f86f7dbeda2b30aeb33641
vikzae <vikzae@gmail.com>
2021-05-14 05:00:13 +0300

    add getAll decks

--- services/clientService.js
+++ services/clientService.js
@@ -1,7 +1,10 @@
+const deck = require("../models/deckModel");
 const deckService = require("./deckService")
 
 let rgxCommandSave = /(?<botCommand>!deck) (?<command>save) (?<gameMode>\w*) (?<deckClass>\w*) (?<deckName>\w*) (?<deckString>\w*) ?(?<comments>.*)/;
 let rgxCommandGet = /(?<botCommand>!deck) (?<command>get) (?<gameMode>\w*) (?<deckName>\w*)/;
+let rgxCommandAll = /(?<botCommand>!deck) (?<command>all) (?<gameMode>\w*)/;
+
 module.exports = (client) => {
     client.on('ready', () => {
         console.log(`Logged in as ${client.user.tag}!`);
@@ -9,23 +12,38 @@ module.exports = (client) => {
 
     client.on('message', (msg) => {
         let message = msg.content;
+
         if (rgxCommandSave.test(message)) {
-            let {gameMode, deckClass, deckName, deckString, comments} = rgxCommandSave.exec(message).groups;
-            // check if it has comments
+            let { gameMode, deckClass, deckName, deckString, comments } = rgxCommandSave.exec(message).groups;
             deckService.save(gameMode, deckClass, deckName, deckString, comments)
                 .then((deck) => {
                     msg.reply(`${deck.deckName} has been saved.`);
                 })
                 .catch((error) => { console.log(error); })
         } else if (rgxCommandGet.test(message)) {
-            let {gameMode, deckName} = rgxCommandGet.exec(message).groups;
-            
+            let { gameMode, deckName } = rgxCommandGet.exec(message).groups;
+
             deckService.get(gameMode, deckName)
                 .then((deck) => {
-                    if (deck == null ) throw 'Deck not Found';
-                    msg.reply(`${deck.deckName} -- ${deck.deckString}`);   
+                    if (deck == null) throw 'Deck not Found';
+                    // check if it has comments
+                    msg.reply(`${deck.deckName} -- ${deck.deckString}`);
                 })
-                .catch((error) => {msg.reply(error)})
+                .catch((error) => { msg.reply(error) })
+        } else if (rgxCommandAll.test(message)) {
+            let { gameMode } = rgxCommandAll.exec(message).groups;
+
+            deckService.all(gameMode)
+                .then((decks) => {
+                    decks.forEach((deck) => {
+                        if (deck.deckComments == '') {
+                            msg.reply(`${deck.deckName} -- ${deck.deckString}`);
+                        } else {
+                            msg.reply(`${deck.deckName} -- ${deck.deckComments} -- ${deck.deckString}`);
+                        }
+                    })
+                })
+                .catch((err) => {msg.reply(err)})
         }
     });
 }
